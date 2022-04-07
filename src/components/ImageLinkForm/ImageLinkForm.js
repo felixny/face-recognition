054069c95cb2f649187466b71513914ea1b5efa8
Felix <nampanya.f@northeastern.edu>
2022-04-06 21:23:46 -0400

    added

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({onInputChange, onSubmit}) => {
     return (
         <div>
             <p className="f3">
-                {'Give it a try'}
+                {'Paste any image links from website. Give it a try'}
             </p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
